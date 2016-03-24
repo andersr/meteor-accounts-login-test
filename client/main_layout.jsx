a95fdf9fa676e98fa7a162ec5f16731e6698c900
Anders Ramsay <andersr@gmail.com>
2016-03-24 17:43:24 -0400

    fixed logout link

--- client/main_layout.jsx
+++ client/main_layout.jsx
@@ -5,7 +5,7 @@ export const MainLayout = ({content}) =>
     <ul>
 		  <li><a href="/signup">Sign Up</a></li>
 		  	  <li><a href="/login">Login</a></li>
-		  <li><a href="/signup">Logout</a></li>
+		  <li><a href="/logout">Logout</a></li>
 		  <li><a href="/about">About</a></li>
 		  <li><a href="/">Home</a></li>
 	  </ul>
